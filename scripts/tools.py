833135f269800fe8ea36590a34ec9f92250ee86f
jjmaldonis <jjmaldonis@gmail.com>
2015-03-19 19:37:24 -0500

    added printTable to tools.py

--- scripts/tools.py
+++ scripts/tools.py
@@ -23,4 +23,15 @@ def makewave():
         of.write('END\n')
     of.close()
 
-
+def printTable(matrix):
+	# The input should be a matrix with col,row. E.g. matrix[0] is the first column to print
+	matrix = [[x if type(x) == type('a') else str(x) for x in col] for col in matrix]
+	rows = len(matrix[0])
+	cols = len(matrix)
+	colSpacing = [ 3+max([len(x) for x in matrix[i]]) for i in range(cols)]
+	table = ''
+	for r in range(rows):
+		for c in range(cols):
+			table = table + matrix[c][r] + ' '*(colSpacing[c]-len(matrix[c][r]))
+		table = table + '\n'
+	return table
